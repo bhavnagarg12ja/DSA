881c551572aa9aec1f2757782fec1f9fa2bcf6ed
Bhavna Garg <bhavnagarg12ja@gmail.com>
2025-01-23 22:30:10 +0530

    Length of Last Word

---
+++ src/Arrays/LengthOfLastWord.java
@@ -0,0 +1,21 @@
+package Arrays;
+
+public class LengthOfLastWord {
+    public static int lengthOfLastWord(String s) {
+        s = s.trim();
+        int length = 0;
+        for(int i = s.length()-1; i>=0; i--){
+            if(s.charAt(i) != ' '){
+                length++;
+            }else if(length > 0){
+                break;
+            }
+        }
+        return length;
+    }
+    public static void main(String[] args) {
+        String s = "   fly me   to   the moon  ";
+        int result = lengthOfLastWord(s);
+        System.out.println(result);
+    }
+}
