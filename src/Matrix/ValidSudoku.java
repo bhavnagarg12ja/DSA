48b0f1bf30a2991320eaf90505084bb0bd680cda
Bhavna Garg <bhavnagarg12ja@gmail.com>
2025-02-13 08:21:31 +0530

    Valid Sudoku

---
+++ src/Matrix/ValidSudoku.java
@@ -0,0 +1,41 @@
+package Matrix;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ValidSudoku {
+    public static boolean isValidSudoku(char[][] board) {
+        Set seen = new HashSet();
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                char num = board[i][j];
+                if (num != '.') {
+                    String row = num + " in row " + i;
+                    String col = num + " in col " + j;
+                    String box = num + " in box " + (i / 3) + "-" + (j / 3);
+                    if (!seen.add(row) || !seen.add(col) || !seen.add(box)) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        char[][] board =
+                {
+                        {'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+                        {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+                        {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+                        {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+                        {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+                        {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+                        {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+                        {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+                        {'.', '.', '.', '.', '8', '.', '.', '7', '9'}
+                };
+        boolean result = isValidSudoku(board);
+        System.out.println(result);
+    }
+}
