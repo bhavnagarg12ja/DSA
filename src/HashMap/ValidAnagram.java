ec123d09b9ec4049d0d0585d480bdbb5be0bd85f
Bhavna Garg <bhavnagarg12ja@gmail.com>
2025-02-18 07:02:07 +0530

    4. IsAnagram

---
+++ src/HashMap/ValidAnagram.java
@@ -0,0 +1,32 @@
+package HashMap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ValidAnagram {
+    public static boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+        for (int i = 0; i < t.length(); i++) {
+            char ch = t.charAt(i);
+            if (!map.containsKey(ch) || map.get(ch) == 0) {
+                return false;
+            }
+            map.put(ch, map.get(ch) - 1);
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String s = "anagram";
+        String t = "nagaram";
+        boolean result = isAnagram(s, t);
+        System.out.println(result);
+    }
+}
