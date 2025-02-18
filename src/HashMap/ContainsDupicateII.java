2577caa3adeb5f0d919fc5ff7f14523142c9be3a
Bhavna Garg <bhavnagarg12ja@gmail.com>
2025-02-18 08:05:32 +0530

    8. Contains Duplicate II

---
+++ src/HashMap/ContainsDupicateII.java
@@ -0,0 +1,25 @@
+package HashMap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ContainsDupicateII {
+    public static boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            if(map.containsKey(nums[i]) && (i - map.get(nums[i]) <= k)){
+                return true;
+            }
+            map.put(nums[i],i);
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,1};
+        int k = 3;
+        boolean result = containsNearbyDuplicate(nums, k);
+        System.out.println(result);
+    }
+
+}
